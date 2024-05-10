be28e1dd81cbe768c7f3fe21fbafd50b63279d6b
minhanhnguyenn <kellyegunnpun0suy@hotmail.com>
2024-05-11 04:20:49 +0700

    start project commit

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
-function bubbleSort(arr) {
-  for (let i = 0; i < arr.length - 1; i++) {
-    for (let j = 0; j < arr.length - i - 1; j++) {
-      if (arr[j] > arr[j + 1]) {
-        let temp = arr[j];
-        arr[j] = arr[j + 1];
-        arr[j + 1] = temp;
-      }
+const binarySearchIterative = (arr, target) => {
+  let left = 0;
+  let right = arr.length - 1;
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2);
+    if (arr[mid] === target) {
+      return mid;
+    } else if (arr[mid] < target) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
     }
   }
-  return arr;
-}
+  return -1;
+};
